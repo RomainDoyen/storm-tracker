847a3a8a91811f9f022d135f46583c65e7dd8481
RomainDoyen <romain.doyen97@gmail.com>
2024-10-06 15:50:25 +0400

    feat: Add leaflet card

---
+++ app/static/js/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([10, 20], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
