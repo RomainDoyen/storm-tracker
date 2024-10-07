3d824eb54d7ebf4b1b886cf359099fcfc1b34166
RomainDoyen <romain.doyen97@gmail.com>
2024-10-07 17:14:57 +0400

    refactor: Classification update

--- app/static/js/data/classification.js
+++ app/static/js/data/classification.js
@@ -1,6 +1,5 @@
 const classificationIcons = {
   'Dépression tropicale': '/static/images/icons/b_TropicalDepression.png',
-  'Dépression tropicale': '/static/images/icons/b_TropicalDepression.png',
   'Tempête tropicale': '/static/images/icons/c_TropicalStorm.png',
   'Ouragan de catégorie 1': '/static/icons/icons/1.png',
   'Ouragan de catégorie 2': '/static/images/icons/2.png',
@@ -12,6 +11,24 @@ const classificationIcons = {
   'Cyclone tropical de catégorie 3': '/static/images/icons/3.png',
   'Cyclone tropical de catégorie 4': '/static/images/icons/4.png',
   'Cyclone tropical de catégorie 5': '/static/images/icons/5.png',
+  'Typhon': '/static/images/icons/typhoon.png',
+  'Super Typhon': '/static/images/icons/typhoon.png',
+  'Perturbation tropicale/dépression/dépression': '/static/images/icons/wind.png',
+  'Zone de basse pression': '/static/images/icons/wind.png',
+  'Dépression': '/static/images/icons/wind.png',
+  'Dépression profonde': '/static/images/icons/wind.png',
+  'Tempête cyclonique': '/static/images/icons/c_TropicalStorm.png',
+  'Tempête cyclonique violente': '/static/images/icons/1.png',
+  'Tempête cyclonique très violente': '/static/images/icons/2.png',
+  'Tempête cyclonique extrêmement violente': '/static/images/icons/3.png',
+  'Tempête super cyclonique': '/static/images/icons/4.png',
+  'Zone de temps perturbé': '/static/images/icons/wind.png',
+  'Perturbation tropicale': '/static/images/icons/wind.png',
+  'Tempête tropicale modérée': '/static/images/icons/c_TropicalStorm.png',
+  'Tempête tropicale sévère': '/static/images/icons/1.png',
+  'Cyclone tropical': '/static/images/icons/2.png',
+  'Cyclone tropical intense': '/static/images/icons/3.png',
+  'Cyclone tropical très intense': '/static/images/icons/4.png',
 };
 
 export default function getIconUrl(classification) {
