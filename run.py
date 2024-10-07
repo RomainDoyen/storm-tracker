60d8076ea254787abe0a8c5caeb6c969bedae085
RomainDoyen <romain.doyen97@gmail.com>
2024-10-07 09:58:46 +0400

    ci: Set up a thread to retrieve data every 10 minutes

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import threading
 from app import app
+from app.routes import update_cyclones_data
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    thread = threading.Thread(target=update_cyclones_data)
+    thread.daemon = True
+    thread.start()
+
     app.run(debug=True, port=7000)
