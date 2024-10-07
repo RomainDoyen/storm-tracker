a183a34b0ddf0c49da43149222e89d3eec655e92
RomainDoyen <romain.doyen97@gmail.com>
2024-10-07 14:20:07 +0400

    ci: Updating host

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':
     thread.daemon = True
     thread.start()
 
-    app.run(debug=True, port=7000)
+    # app.run(debug=True, port=7000)
+    app.run(debug=False, host='0.0.0.0')
